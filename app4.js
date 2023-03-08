c98b11629a04c2fa9d1141fabc61c64a03d65c10
omegamaster86 <masya868686@gmail.com>
2023-03-08 23:47:35 +0900

    JS課題1-4修正致しました。

--- app4.js
+++ app4.js
@@ -1,41 +1,40 @@
-
 const newTask1 = prompt("タスク入力して下さい");
-//const newTask2 = prompt("ジャンルを入力してください");//こいつが原因
 const tasks = [
-{content:'0 :[内容]机を片付ける',
-genre:'[ジャンル] 掃除'},
-
-{content:'1 :[内容]牛乳を買う',
-genre:'[ジャンル]買い物'},
-
-{content:'2 :[内容]散歩する',
-genre:'[ジャンル]運動'},
-]
-
-console.log("=======================\n現在持っているタスク一覧\n=======================");
-for (const todo of tasks) {
-console.log(todo.content+','+todo.genre);
-}
+    {content:'机を片付ける',
+    genre:'掃除'},
+    
+    {content:'牛乳を買う',
+    genre:'買い物'},
+    
+    {content:'散歩する',
+    genre:'運動'},
+    ]
+    
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
+    }
 
 function fn () {
-if (newTask1 !== null) {
-    const newTask2 = prompt("ジャンルを入力してください");
-    const newTask = {content:'3 :[内容]'+ newTask1,genre:'[ジャンル]' + newTask2}
+    if (newTask1 !== null) {
+        const newTask2 = prompt("ジャンルを入力してください");
+        const newTask = {content:newTask1,genre:newTask2}
+    
+        tasks.push(newTask);
+    }}
+    fn();
+    
+    
+    if (newTask1 !== null) {
+    alert("新しいタスクを追加しました。");
+    } 
+    
+    console.log("=======================\n現在持っているタスク一覧\n=======================");
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
+    }
+    
+    if (newTask1 !== null) {
+    alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
+    }
 
-    tasks.push(newTask);
-}}
-fn();
 
-
-if (newTask1 !== null) {
-alert("新しいタスクを追加しました。");
-} 
-
-console.log("=======================\n現在持っているタスク一覧\n=======================");
-for (const todo of tasks) {
-console.log(todo.content+','+todo.genre);
-}
-
-if (newTask1 !== null) {
-alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
-}
