38dbfae7abdd80cfab20747edbe86e69cc413642
omegamaster86 <masya868686@gmail.com>
2023-03-11 17:58:52 +0900

    pritter使用済み、クラス名変更

--- app4.js
+++ app4.js
@@ -1,36 +1,36 @@
 const task = prompt("タスク入力して下さい");
 const tasks = [
-{ content: "机を片付ける", genre: "掃除" },
+  { content: "机を片付ける", genre: "掃除" },
 
-{ content: "牛乳を買う", genre: "買い物" },
+  { content: "牛乳を買う", genre: "買い物" },
 
-{ content: "散歩する", genre: "運動" },
+  { content: "散歩する", genre: "運動" },
 ];
 
 for (let i = 0; i < tasks.length; i++) {
-console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
+  console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
 }
 
 function fn() {
-if (newTask1 !== null) {
+  if (newTask1 !== null) {
     const genre = prompt("ジャンルを入力してください");
     const newTask = { content: task, genre: genre };
     tasks.push(newTask);
-}
+  }
 }
 fn();
 
 if (newTask1 !== null) {
-alert("新しいタスクを追加しました。");
+  alert("新しいタスクを追加しました。");
 }
 
 console.log(
-"=======================\n現在持っているタスク一覧\n======================="
+  "=======================\n現在持っているタスク一覧\n======================="
 );
 for (let i = 0; i < tasks.length; i++) {
-console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
+  console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
 }
 
 if (newTask1 !== null) {
-alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
+  alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
 }
