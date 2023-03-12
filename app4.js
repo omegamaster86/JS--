74c54021673a13bc8bb5781d3e6d4d5ce76a1f1f
omegamaster86 <masya868686@gmail.com>
2023-03-12 12:16:34 +0900

    ご指摘の箇所を修正致しました。

--- app4.js
+++ app4.js
@@ -12,7 +12,7 @@ for (let i = 0; i < tasks.length; i++) {
 }
 
 function fn() {
-  if (newTask1 !== null) {
+  if (task !== null) {
     const genre = prompt("ジャンルを入力してください");
     const newTask = { content: task, genre: genre };
     tasks.push(newTask);
@@ -20,7 +20,7 @@ function fn() {
 }
 fn();
 
-if (newTask1 !== null) {
+if (task !== null) {
   alert("新しいタスクを追加しました。");
 }
 
@@ -31,6 +31,6 @@ for (let i = 0; i < tasks.length; i++) {
   console.log(`${i}:[内容]${tasks[i].content},[ジャンル]${tasks[i].genre}`);
 }
 
-if (newTask1 !== null) {
+if (task !== null) {
   alert("「確認、追加、削除、終了」の４つのいずれかを入力してください。");
 }
